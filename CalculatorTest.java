4e546d2a642e91f9281751e05bdbdf1bd43c5e24
Sandeep Chintala <chintalasandeep915@gmail.com>
2025-11-03 00:02:17 +0530

    savefiles

---
+++ CalculatorTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test; 
+import static org.junit.Assert.assertEquals; 
+public class CalculatorTest { 
+    Calculator calc = new Calculator(); 
+    @Test public void testAddition() { assertEquals(15, calc.add(10, 5)); } 
+    @Test public void testSubtraction() { assertEquals(5, calc.subtract(10, 5)); } 
+    @Test public void testMultiplication() { assertEquals(50, calc.multiply(10, 5)); } 
+    @Test public void testDivision() { assertEquals(2, calc.divide(10, 5)); }} 
